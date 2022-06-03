ec3ee34d1ccd6779ccdbe64d4058d46b199c662c
junjun <1902958347@qq.com>
2022-06-03 18:38:35 +0800

    request中环境变量和异常的处理

--- src/utils/request.js
+++ src/utils/request.js
@@ -1,7 +1,25 @@
 import axios from 'axios'
-const service = axios.create()
+import { Message } from 'element-ui'
+const service = axios.create({
+  baseURL: process.env.VUE_APP_BASE_API,
+  timeout: 5000 // 设置超时时间
+})
 // 1.请求拦截器
 service.interceptors.request.use()
 // 2.响应拦截器
-service.interceptors.response.use()
+service.interceptors.response.use(response => {
+  // axios默认加了一层data
+  const { success, message, data } = response.data
+  // 要根据success的成功与否决定下面的操作
+  if (success) {
+    return data
+  } else {
+    // 业务已经错误了 还能进then？ 不能！ 应该进catch
+    Message.error(message)
+    return Promise.reject(new Error(message))
+  }
+}, error => {
+  Message.error(error.message)
+  return Promise.reject(error) // 返回执行错误 让当前的执行链跳出成功 进入catch
+})
 export default service
