527c1ea8e8ecf2c126303d71d0b0c7ce43127102
junjun <1902958347@qq.com>
2022-06-05 16:47:34 +0800

    主页的token拦截处理

--- src/permission.js
+++ src/permission.js
@@ -1,1 +1,32 @@
 // 控制页面登录权限
+import router from './router'
+import store from './store'
+import NProgress from 'nprogress'
+import 'nprogress/nprogress.css'
+
+// 定义白名单
+const WhiteList = ['/login', '/404']
+
+// 前置守卫
+router.beforeEach(function(to, from, next) {
+  NProgress.start()
+  if (store.getters.token) {
+    if (to.path === '/login') {
+      next('/')
+    } else {
+      next()
+    }
+  } else {
+    if (WhiteList.indexOf(to.path) > -1) {
+      next()
+    } else {
+      next('/')
+    }
+  }
+  // NProgress.done()
+})
+
+// 后置守卫
+router.afterEach(function() {
+  NProgress.done()
+})
