4bd21e41bd287b0ce8708bbf73bed6e78facbefe
junjun <1902958347@qq.com>
2022-06-10 10:09:38 +0800

    主页token拦截处理’ git commit -m 主页token拦截处理’

--- src/permission.js
+++ src/permission.js
@@ -1,6 +1,6 @@
 // 控制页面登录权限
-import router from './router'
-import store from './store'
+import router from '@/router'
+import store from '@/store'
 import NProgress from 'nprogress'
 import 'nprogress/nprogress.css'
 
@@ -8,6 +8,10 @@ import 'nprogress/nprogress.css'
 const WhiteList = ['/login', '/404']
 
 // 前置守卫
+// next是前置守卫必须必须执行的钩子，next必须执行，如果不执行页面就死了
+// next（）放过
+// next（false）跳转终止
+// next（地址）跳至地址
 router.beforeEach(function(to, from, next) {
   NProgress.start()
   if (store.getters.token) {
@@ -20,10 +24,11 @@ router.beforeEach(function(to, from, next) {
     if (WhiteList.indexOf(to.path) > -1) {
       next()
     } else {
-      next('/')
+      console.log()
+      next('/login')
     }
   }
-  // NProgress.done()
+  NProgress.done() // 强关一次，解决手动跳转时进度条不停的问题
 })
 
 // 后置守卫
