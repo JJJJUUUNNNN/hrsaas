0bb9f7c2379947e911435c1023df17cf844c7066
junjun <1902958347@qq.com>
2022-06-13 11:15:12 +0800

    将树形内容单独封装成组件简化代码

--- src/views/departments/index.vue
+++ src/views/departments/index.vue
@@ -5,61 +5,13 @@
       <!--组织架构的内容-->
       <el-card class="tree-card">
         <!-- 头部 -->
-        <el-row type="flex" justify="space-between" align="middle" style="height:40px;">
-          <!-- 左 -->
-          <el-col>
-            <span>玛卡巴卡科技股份有限公司</span>
-          </el-col>
-          <!-- 右 -->
-          <el-col :span="4">
-            <el-row type="flex" justify="end">
-              <el-col>负责人</el-col>
-              <!-- 下拉菜单 -->
-              <el-col>
-                <el-dropdown>
-                  <span>
-                    操作
-                    <i class="el-icon-arrow-down" />
-                  </span>
-                  <el-dropdown-menu slot="dropdown">
-                    <el-dropdown-item>添加子部门</el-dropdown-item>
-                  </el-dropdown-menu>
-                </el-dropdown>
-              </el-col>
-            </el-row>
-          </el-col>
-        </el-row>
+        <tree-tools :tree-node="company" :is-root="true" />
 
         <!-- 树形结构 -->
         <el-tree :data="departs" :props="defaultProps" :default-expand-all="true">
           <!-- 作用域插槽 slot-scope："obj" -->
           <!-- 传入内容 插槽内容 有几个结点，就循环几次 data:每个节点的数据对象-->
-          <el-row slot-scope="{data}" type="flex" justify="space-between" align="middle" style="height:40px; width:100%">
-            <!-- 左 -->
-            <el-col>
-              <span>{{ data.name }}</span>
-            </el-col>
-            <!-- 右 -->
-            <el-col :span="4">
-              <el-row type="flex" justify="end">
-                <el-col>{{ data.manager }}</el-col>
-                <!-- 下拉菜单 -->
-                <el-col>
-                  <el-dropdown>
-                    <span>
-                      操作
-                      <i class="el-icon-arrow-down" />
-                    </span>
-                    <el-dropdown-menu slot="dropdown">
-                      <el-dropdown-item>添加子部门</el-dropdown-item>
-                      <el-dropdown-item>编辑部门</el-dropdown-item>
-                      <el-dropdown-item>删除部门</el-dropdown-item>
-                    </el-dropdown-menu>
-                  </el-dropdown>
-                </el-col>
-              </el-row>
-            </el-col>
-          </el-row>
+          <tree-tools slot-scope="{data}" :tree-node="data" />
 
         </el-tree>
       </el-card>
@@ -69,7 +21,12 @@
 </template>
 
 <script>
+import TreeTools from './components/tree-tools.vue'
+
 export default {
+  components: {
+    TreeTools
+  },
   data() {
     return {
       company: { name: '玛卡巴卡科技股份有限公司', manager: '负责人' },
