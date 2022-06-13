1c8099764a2928f103842193a5fe2316afce6264
junjun <1902958347@qq.com>
2022-06-13 10:40:30 +0800

    实现树形的静态组织架构

--- src/views/departments/index.vue
+++ src/views/departments/index.vue
@@ -1,25 +1,26 @@
 <template>
   <div class="dashboard-container">
     <div class="app-container">
+
+      <!--组织架构的内容-->
       <el-card class="tree-card">
-        <!-- 用了一个行列布局 头部结构-->
-        <el-row type="flex" justify="space-between" align="middle" style="height: 40px">
+        <!-- 头部 -->
+        <el-row type="flex" justify="space-between" align="middle" style="height:40px;">
           <!-- 左 -->
           <el-col>
-            <span>江苏传智播客教育科技股份有限公司</span>
+            <span>玛卡巴卡科技股份有限公司</span>
           </el-col>
           <!-- 右 -->
           <el-col :span="4">
             <el-row type="flex" justify="end">
-              <!-- 两个内容 -->
               <el-col>负责人</el-col>
+              <!-- 下拉菜单 -->
               <el-col>
-                <!-- 下拉菜单 element -->
                 <el-dropdown>
                   <span>
-                    操作<i class="el-icon-arrow-down" />
+                    操作
+                    <i class="el-icon-arrow-down" />
                   </span>
-                  <!-- 下拉菜单 -->
                   <el-dropdown-menu slot="dropdown">
                     <el-dropdown-item>添加子部门</el-dropdown-item>
                   </el-dropdown-menu>
@@ -30,21 +31,10 @@
         </el-row>
 
         <!-- 树形结构 -->
-        <!--放置一个属性   这里的props和我们之前学习的父传子 的props没关系-->
-        <el-tree
-          :data="departs"
-          :props="defaultProps"
-          :default-expand-all="true"
-        >
-          <!-- 传入内容 插槽内容 会循环多次 有多少节点 就循环多少次 -->
-          <!-- 作用域插槽 slot-scope="obj" 接收传递给插槽的数据   data 每个节点的数据对象-->
-          <el-row
-            slot-scope="{data}"
-            type="flex"
-            justify="space-between"
-            align="middle"
-            style="height: 40px; width:100%"
-          >
+        <el-tree :data="departs" :props="defaultProps" :default-expand-all="true">
+          <!-- 作用域插槽 slot-scope："obj" -->
+          <!-- 传入内容 插槽内容 有几个结点，就循环几次 data:每个节点的数据对象-->
+          <el-row slot-scope="{data}" type="flex" justify="space-between" align="middle" style="height:40px; width:100%">
             <!-- 左 -->
             <el-col>
               <span>{{ data.name }}</span>
@@ -52,16 +42,14 @@
             <!-- 右 -->
             <el-col :span="4">
               <el-row type="flex" justify="end">
-                <!-- 两个内容 -->
                 <el-col>{{ data.manager }}</el-col>
+                <!-- 下拉菜单 -->
                 <el-col>
-                  <!-- 下拉菜单 element -->
                   <el-dropdown>
                     <span>
                       操作
                       <i class="el-icon-arrow-down" />
                     </span>
-                    <!-- 下拉菜单  具名插槽-->
                     <el-dropdown-menu slot="dropdown">
                       <el-dropdown-item>添加子部门</el-dropdown-item>
                       <el-dropdown-item>编辑部门</el-dropdown-item>
@@ -71,10 +59,11 @@
                 </el-col>
               </el-row>
             </el-col>
-
           </el-row>
+
         </el-tree>
       </el-card>
+
     </div>
   </div>
 </template>
@@ -83,11 +72,11 @@
 export default {
   data() {
     return {
+      company: { name: '玛卡巴卡科技股份有限公司', manager: '负责人' },
       defaultProps: {
         label: 'name' // 表示 从这个属性显示内容
       },
-      departs: [
-        { name: '总裁办', manager: '曹操', children: [{ name: '董事会', manager: '曹丕' }] },
+      departs: [{ name: '总裁办', manager: '曹操', children: [{ name: '董事会', manager: '曹丕' }] },
         { name: '行政部', manager: '刘备' },
         { name: '人事部', manager: '孙权' }]
     }
